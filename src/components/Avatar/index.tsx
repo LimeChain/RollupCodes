0cca630aec84a7afc97ba7692a8c03340c2a86e9
svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
2023-06-22 08:22:32 +0300

    avatar component changes

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -2,31 +2,42 @@ import Typography from '@components/Typography'
 import { AvatarSize, Headings, Text, Font } from '@utils/types'
 import styles from './styles.module.scss'
 import classNames from 'classnames'
+import Image from 'next/image'
 
 interface IAvatar {
     name: string
+    src: string
     size?: AvatarSize
 }
 
-const Avatar = ({ name = '', size = AvatarSize.SMALL }: IAvatar) => {
+const Avatar = ({ name = '', size = AvatarSize.SMALL, src }: IAvatar) => {
     const isLarge = Boolean(AvatarSize.LARGE === size)
 
     return (
         <div className={styles.avatar}>
-            <div
-                className={classNames(styles.circle, {
-                    [styles.largeCircle]: isLarge,
-                })}
-            >
-                <Typography
-                    variant={isLarge ? Headings.H5 : Text.BODY2}
-                    fontWeight="500"
-                    fontStyle={'italic'}
-                    color="var(--neutral)"
+            {src ? (
+                <Image
+                    src={src}
+                    alt={name}
+                    width={isLarge ? 48 : 32}
+                    height={isLarge ? 48 : 32}
+                />
+            ) : (
+                <div
+                    className={classNames(styles.circle, {
+                        [styles.largeCircle]: isLarge,
+                    })}
                 >
-                    {name.slice(0, 2).toUpperCase()}
-                </Typography>
-            </div>
+                    <Typography
+                        variant={isLarge ? Headings.H5 : Text.BODY2}
+                        fontWeight="500"
+                        fontStyle={'italic'}
+                        color="var(--opposite-text-color)"
+                    >
+                        {name.slice(0, 1).toUpperCase()}
+                    </Typography>
+                </div>
+            )}
             <Typography
                 variant={Headings[isLarge ? 'H3' : 'H4']}
                 fontWeight="700"
