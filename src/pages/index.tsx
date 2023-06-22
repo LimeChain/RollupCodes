84ebc2f400594ba5728d47e2073169d4bab781a3
svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
2023-06-22 08:45:42 +0300

    homepage changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Layout from '@components/Layout'
 import Typography from '@components/Typography'
-import { Headings, Text, IRollupMeta, Font } from '@utils/types'
+import { Headings, Text, Font, IDocMeta } from '@utils/types'
 import RollupSummaryCard from '@components/RollupSummaryCard'
 import Grid from '@components/Grid'
 import fs from 'fs'
@@ -9,14 +9,14 @@ import matter from 'gray-matter'
 import getConfig from 'next/config'
 
 interface IHomeProps {
-    rollups: IRollupMeta[]
+    docs: IDocMeta[]
 }
 
 const { serverRuntimeConfig } = getConfig()
 
-export default function Home({ rollups }: IHomeProps) {
+export default function Home({ docs }: IHomeProps) {
     return (
-        <Layout>
+        <Layout loading={docs?.length === 0}>
             <Typography
                 variant={Headings.H2}
                 fontWeight="300"
@@ -52,8 +52,8 @@ export default function Home({ rollups }: IHomeProps) {
                 in-depth analyses of the expanding Ethereum ecosystem
             </Typography>
             <Grid>
-                {rollups?.map((meta: IRollupMeta, index: number) => (
-                    <RollupSummaryCard key={`rollup-${index}`} {...meta} />
+                {docs?.map((docMeta: IDocMeta, index: number) => (
+                    <RollupSummaryCard key={`rollup-${index}`} {...docMeta} />
                 ))}
             </Grid>
         </Layout>
@@ -61,30 +61,32 @@ export default function Home({ rollups }: IHomeProps) {
 }
 
 export const getStaticProps = async () => {
-    const docsPath = join(serverRuntimeConfig.APP_ROOT, 'src/docs/rollups')
-    const docs = fs.readdirSync(docsPath)
+    const docs = await getDocsMetadata()
+    return { props: { docs } }
+}
 
-    let rollups: IRollupMeta[] = []
+const getDocsMetadata = async () => {
+    const folder = 'src/docs/'
+    const path = join(serverRuntimeConfig.APP_ROOT, folder)
+    const files = fs.readdirSync(path)
+    const markdownDocs = files.filter((file) => file.endsWith('.mdx'))
+
+    // Get gray-matter data from each file.
+    const docs: IDocMeta[] = []
 
     await Promise.all(
-        docs.map(async (doc) => {
-            try {
-                const markdownWithMeta = fs.readFileSync(
-                    join(docsPath, doc),
-                    'utf-8'
-                )
-                const { data } = matter(markdownWithMeta)
-                if (data) {
-                    rollups.push(data as IRollupMeta)
-                }
-            } catch (error) {
-                console.debug(
-                    "Couldn't read the Markdown doc for the opcode",
-                    error
-                )
-            }
+        markdownDocs.map((fileName) => {
+            const fileContents = fs.readFileSync(`${folder}${fileName}`, 'utf8')
+            const matterResult = matter(fileContents)
+            docs.push({
+                title: matterResult?.data?.title,
+                logo: `images/${fileName.replace('.mdx', '')}-logo.svg`,
+                subtitle: matterResult?.data?.subtitle,
+                slug: fileName.replace('.mdx', ''),
+                labels: matterResult?.data?.labels,
+            })
         })
     )
 
-    return { props: { rollups } }
+    return docs
 }
