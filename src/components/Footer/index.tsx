67367895ddaee12099cb8e346d361565550c059b
Daniel-K-Ivanov <daniel.k.ivanov95@gmail.com>
2023-06-27 17:42:49 +0200

    fix(link): homepage license
    Signed-off-by: Daniel-K-Ivanov <daniel.k.ivanov95@gmail.com>

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 import Typography from '@components/Typography'
 import styles from './styles.module.scss'
-import { Text } from '@utils/types'
+import {Text} from '@utils/types'
 import Link from 'next/link'
 import PlusIcon from 'public/images/plus-icon.svg'
 import TwitterIcon from 'public/images/twitter-icon.svg'
@@ -9,68 +9,74 @@ import LimechainLogo from 'public/images/limechain-logo.svg'
 import GithubIcon from 'public/images/github-icon.svg'
 
 const Footer = () => {
-    return (
-        <div className={styles.footer}>
-            <div className={styles.group}>
-                <Link
-                    href="https://limechain.tech/"
-                    target="_target"
-                    className={styles.defaultButton}
-                >
-                    <Typography
-                        variant={Text.BODY2}
-                        fontWeight={'400'}
-                        marginRight={5}
-                    >
-                        Made by
-                    </Typography>
-                    <LimechainLogo fill="var(--icon-color)" />
-                </Link>
-                <div className={styles.pipe}>{'|'}</div>
-                <Link
-                    href={'https://twitter.com/LimeChainHQ'}
-                    target="_target"
-                    className={styles.link}
-                >
-                    <TwitterIcon fill="var(--icon-color)" />
-                </Link>
-                <Link
-                    href={'https://github.com/LimeChain/rollup-codes'}
-                    target="_target"
-                    className={styles.link}
-                >
-                    <GithubIcon fill="var(--icon-color)" />
-                </Link>
-                <Link
-                    href={'mailto:rollup.codes@limechain.tech'}
-                    target="_target"
-                    className={styles.link}
-                >
-                    <EmailIcon fill="var(--icon-color)" />
-                </Link>
-            </div>
-            <div className={styles.group}>
-                <Typography variant={Text.BODY2} fontWeight="400">
-                    Open-sourced under [MIT license](https://github.com/LimeChain/rollup-codes/blob/main/LICENSE)
-                </Typography>
-                <div className={styles.pipe}>{'|'}</div>
-                <Link
-                    href="https://github.com/LimeChain/rollup-codes"
-                    target="_blank"
-                    className={styles.defaultButton}
-                >
-                    <Typography
-                        variant={Text.BODY2}
-                        fontWeight={'700'}
-                        marginRight={4}
-                    >
-                        Contribute on Github
-                    </Typography>
-                    <PlusIcon fill="var(--icon-color)" />
-                </Link>
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.footer}>
+      <div className={styles.group}>
+        <Link
+          href="https://limechain.tech/"
+          target="_target"
+          className={styles.defaultButton}
+        >
+          <Typography
+            variant={Text.BODY2}
+            fontWeight={'400'}
+            marginRight={5}
+          >
+            Made by
+          </Typography>
+          <LimechainLogo fill="var(--icon-color)"/>
+        </Link>
+        <div className={styles.pipe}>{'|'}</div>
+        <Link
+          href={'https://twitter.com/LimeChainHQ'}
+          target="_target"
+          className={styles.link}
+        >
+          <TwitterIcon fill="var(--icon-color)"/>
+        </Link>
+        <Link
+          href={'https://github.com/LimeChain/rollup-codes'}
+          target="_target"
+          className={styles.link}
+        >
+          <GithubIcon fill="var(--icon-color)"/>
+        </Link>
+        <Link
+          href={'mailto:rollup.codes@limechain.tech'}
+          target="_target"
+          className={styles.link}
+        >
+          <EmailIcon fill="var(--icon-color)"/>
+        </Link>
+      </div>
+      <div className={styles.group}>
+        <Link
+          href="https://github.com/LimeChain/rollup-codes/blob/main/LICENSE"
+          target="_blank"
+          className={styles.defaultButton}
+        >
+          <Typography variant={Text.BODY2} fontWeight="400">
+            Open-sourced under MIT license
+          </Typography>
+        </Link>
+        <div className={styles.pipe}>{'|'}</div>
+        <Link
+          href="https://github.com/LimeChain/rollup-codes"
+          target="_blank"
+          className={styles.defaultButton}
+        >
+          <Typography
+            variant={Text.BODY2}
+            fontWeight={'700'}
+            marginRight={4}
+          >
+            Contribute on Github
+          </Typography>
+          <PlusIcon fill="var(--icon-color)"/>
+        </Link>
+      </div>
+    </div>
+  )
 }
 
 export default Footer
