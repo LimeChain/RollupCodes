500ef788cb65fa8374e4670c57fdb033070fb617
svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
2023-06-27 13:40:21 +0300

    #12 - formatting for emphasised text

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import '@styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
-import { Inter, Chakra_Petch } from 'next/font/google'
+import { Inter, Chakra_Petch, Roboto_Mono } from 'next/font/google'
 import classNames from 'classnames'
+
+const robotoMono = Roboto_Mono({
+    subsets: ['latin'],
+    style: 'normal',
+    weight: ['100', '200', '300', '400', '500', '600', '700'],
+    variable: '--font-roboto-mono',
+})
 
 const chakraPetch = Chakra_Petch({
     subsets: ['latin'],
@@ -24,6 +31,7 @@ export default function App({ Component, pageProps }: AppProps) {
             <style jsx global>{`
                 :root {
                     --font-chakra-petch: ${chakraPetch.style.fontFamily};
+                    --font-roboto-mono: ${robotoMono.style.fontFamily};
                 }
                 html {
                     font-family: ${inter.style.fontFamily};
