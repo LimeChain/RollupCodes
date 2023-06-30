5d148433285abeea03d70fd4160e7fbb6ac39abb
svetoslav-nikol0v <136077184+svetoslav-nikol0v@users.noreply.github.com>
2023-06-30 13:25:58 +0300

    Meta tag (#42)
    * tag
    
    * image
    
    * image path
    
    * move head section
    
    * image
    
    * use origin
    
    * head
    
    * comment out all meta tags
    
    * Head tag from nextjs
    
    * og tags
    
    * twitter card tag
    
    * twitter card tag
    
    * move tags to _app
    
    * fix
    
    * image path
    
    * add https
    
    * vercel variable
    
    * variable
    
    * remove log

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
 import { Inter, Chakra_Petch, Roboto_Mono } from 'next/font/google'
 import classNames from 'classnames'
+import Head from 'next/head'
 
 const robotoMono = Roboto_Mono({
     subsets: ['latin'],
@@ -32,6 +33,62 @@ export default function App({ Component, pageProps }: AppProps) {
             defaultTheme="dark"
             themes={['light', 'dark']}
         >
+            <Head>
+                <link rel="icon" href="/images/favicon.ico" />
+
+                <title>RollupCodes</title>
+                <meta
+                    name="description"
+                    content="Rollup Codes: Interactive Reference of the Ethereum Rollup Ecosystem. A comprehensive tool for developers to compare and and do in-depth analysis of the expanding Ethereum ecosystem"
+                />
+
+                <meta
+                    property="og:url"
+                    content="https://rollup.codes"
+                    key="ogurl"
+                />
+                <meta property="og:type" content="website" />
+                <meta
+                    property="og:title"
+                    content="RollupCodes: Interactive Reference of the Ethereum Rollup Ecosystem"
+                    key="ogtitle"
+                />
+                <meta
+                    property="og:description"
+                    content="A comprehensive tool for developers to compare and and do in-depth analysis of the expanding Ethereum ecosystem"
+                    key="ogdesc"
+                />
+                <meta
+                    property="og:image"
+                    content={`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/images/thumbnail.png`}
+                    key="ogimage"
+                />
+                <meta property="og:image:width" content="1200" />
+                <meta property="og:image:height" content="630" />
+
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta property="twitter:domain" content="rollup.codes" />
+                <meta property="twitter:url" content="https://rollup.codes" />
+                <meta
+                    name="twitter:title"
+                    content="RollupCodes: Interactive Reference of the Ethereum Rollup Ecosystem"
+                />
+                <meta
+                    name="twitter:description"
+                    content="A comprehensive tool for developers to compare and and do in-depth analysis of the expanding Ethereum ecosystem"
+                />
+                <meta
+                    name="twitter:image"
+                    content={`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/images/thumbnail.png`}
+                />
+
+                <meta name="robots" content="index, follow" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <script src="/js/googleAnalytics.js" async />
+            </Head>
             <style jsx global>{`
                 :root {
                     --font-chakra-petch: ${chakraPetch.style.fontFamily};
