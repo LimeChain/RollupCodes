e75a612f933d0f88ffcf3fcb55a766b397fdf830
svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
2023-07-03 08:46:48 +0300

    attempt 3

--- src/components/GoogleAnalytics/index.tsx
+++ src/components/GoogleAnalytics/index.tsx
@@ -7,8 +7,7 @@ const Googleanalytics = () => {
 
     useEffect(() => {
         if (typeof window != 'undefined') {
-            console.log(window.location.origin)
-            console.log(window.location.origin)
+            console.log(window.location)
 
             setIsProductionOrigin(
                 window.location.origin === 'https://www.rollup.codes'
@@ -16,30 +15,40 @@ const Googleanalytics = () => {
         }
     }, [])
 
+    if (!isProduction || !isProductionOrigin) {
+        return null
+    }
+
     return (
         <>
-            {isProduction && isProductionOrigin && (
-                <Script
-                    id="ga"
-                    dangerouslySetInnerHTML={{
-                        __html: `
-                    (function(w, d, s, l, i) {
-                        w[l] = w[l] || [];
-                        w[l].push({
-                            'gtm.start': new Date().getTime(),
-                            event: 'gtm.js'
-                        });
-                        var f = d.getElementsByTagName(s)[0],
-                            j = d.createElement(s),
-                            dl = l != 'dataLayer' ? '&l=' + l : '';
-                        j.async = true;
-                        j.src =
-                            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-                        f.parentNode.insertBefore(j, f);
-                    })(window, document, 'script', 'dataLayer', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID}');`,
-                    }}
+            <noscript>
+                <iframe
+                    src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+                    height="0"
+                    width="0"
+                    style={{ display: 'none', visibility: 'hidden' }}
                 />
-            )}
+            </noscript>
+            <Script
+                id="google-analytics"
+                dangerouslySetInnerHTML={{
+                    __html: `
+                        (function(w, d, s, l, i) {
+                            w[l] = w[l] || [];
+                            w[l].push({
+                                'gtm.start': new Date().getTime(),
+                                event: 'gtm.js'
+                            });
+                            var f = d.getElementsByTagName(s)[0],
+                                j = d.createElement(s),
+                                dl = l != 'dataLayer' ? '&l=' + l : '';
+                            j.async = true;
+                            j.src =
+                                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+                            f.parentNode.insertBefore(j, f);
+                        })(window, document, 'script', 'dataLayer', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');`,
+                }}
+            />
         </>
     )
 }
