4852d7339fd9970586fa9b6b704933033e5c79b7
svetoslav-nikol0v <136077184+svetoslav-nikol0v@users.noreply.github.com>
2023-07-05 11:28:07 +0300

    Add google analytics measurement id (#49)
    * add google analytics measurement id
    
    * attemp
    
    * id
    
    * env.local
    
    * ga only on prod
    
    * add GoogleAnalytics component
    
    * attempt
    
    * mode log
    
    * attempt 2
    
    * add log
    
    * fix
    
    * attempt 3
    
    * add ga as high as possible
    
    * adjustments
    
    * remove
    
    * remove log
    
    * add generated Git branch URL
    
    * adding log
    
    * using vercel's public env variable
    
    ---------
    
    Co-authored-by: Daniel Ivanov <Daniel.K.Ivanov95@gmail.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from 'next-themes'
 import { Inter, Chakra_Petch, Roboto_Mono } from 'next/font/google'
 import classNames from 'classnames'
 import Head from 'next/head'
+import Googleanalytics from '@components/GoogleAnalytics'
 
 const robotoMono = Roboto_Mono({
     subsets: ['latin'],
@@ -28,79 +29,87 @@ const inter = Inter({
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            themes={['light', 'dark']}
-        >
-            <Head>
-                <link rel="icon" href="/images/favicon.ico" />
-
-                <title>RollupCodes</title>
-                <meta
-                    name="description"
-                    content="Rollup Codes: Interactive Reference of the Ethereum Rollup Ecosystem. A comprehensive tool for developers to compare and and do in-depth analysis of the expanding Ethereum ecosystem"
-                />
-
-                <meta
-                    property="og:url"
-                    content="https://rollup.codes"
-                    key="ogurl"
-                />
-                <meta property="og:type" content="website" />
-                <meta
-                    property="og:title"
-                    content="RollupCodes: Interactive Reference of the Ethereum Rollup Ecosystem"
-                    key="ogtitle"
-                />
-                <meta
-                    property="og:description"
-                    content="A comprehensive tool for developers to compare and and do in-depth analysis of the expanding Ethereum ecosystem"
-                    key="ogdesc"
-                />
-                <meta
-                    property="og:image"
-                    content={`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/images/thumbnail.png`}
-                    key="ogimage"
-                />
-                <meta property="og:image:width" content="1200" />
-                <meta property="og:image:height" content="630" />
-
-                <meta name="twitter:card" content="summary_large_image" />
-                <meta property="twitter:domain" content="rollup.codes" />
-                <meta property="twitter:url" content="https://rollup.codes" />
-                <meta
-                    name="twitter:title"
-                    content="RollupCodes: Interactive Reference of the Ethereum Rollup Ecosystem"
-                />
-                <meta
-                    name="twitter:description"
-                    content="A comprehensive tool for developers to compare and and do in-depth analysis of the expanding Ethereum ecosystem"
-                />
-                <meta
-                    name="twitter:image"
-                    content={`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/images/thumbnail.png`}
-                />
-
-                <meta name="robots" content="index, follow" />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1"
-                />
-                <script src="/js/googleAnalytics.js" async />
-            </Head>
-            <style jsx global>{`
-                :root {
-                    --font-chakra-petch: ${chakraPetch.style.fontFamily};
-                    --font-roboto-mono: ${robotoMono.style.fontFamily};
-                }
-                html {
-                    font-family: ${inter.style.fontFamily};
-                }
-            `}</style>
-            <main className={classNames('main')}>
-                <Component {...pageProps} />
-            </main>
-        </ThemeProvider>
+        <>
+            <Googleanalytics />
+            <ThemeProvider
+                attribute="class"
+                defaultTheme="dark"
+                themes={['light', 'dark']}
+            >
+                <Head>
+                    <link rel="icon" href="/images/favicon.ico" />
+                    <title>RollupCodes</title>
+                    <meta
+                        name="description"
+                        content="Rollup Codes: Interactive Reference of the Ethereum Rollup Ecosystem. A comprehensive tool for developers to compare and and do in-depth analysis of the expanding Ethereum ecosystem"
+                    />
+                    <meta
+                        property="og:url"
+                        content="https://rollup.codes"
+                        key="ogurl"
+                    />
+                    <meta property="og:type" content="website" />
+                    <meta
+                        property="og:title"
+                        content="RollupCodes: Interactive Reference of the Ethereum Rollup Ecosystem"
+                        key="ogtitle"
+                    />
+                    <meta
+                        property="og:description"
+                        content="A comprehensive tool for developers to compare and and do in-depth analysis of the expanding Ethereum ecosystem"
+                        key="ogdesc"
+                    />
+                    <meta
+                        property="og:image"
+                        content={`https://${
+                            process.env.NEXT_PUBLIC_VERCEL_URL ||
+                            process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL
+                        }/images/thumbnail.png`}
+                        key="ogimage"
+                    />
+                    <meta property="og:image:width" content="1200" />
+                    <meta property="og:image:height" content="630" />
+                    <meta name="twitter:card" content="summary_large_image" />
+                    <meta property="twitter:domain" content="rollup.codes" />
+                    <meta
+                        property="twitter:url"
+                        content="https://rollup.codes"
+                    />
+                    <meta
+                        name="twitter:title"
+                        content="RollupCodes: Interactive Reference of the Ethereum Rollup Ecosystem"
+                    />
+                    <meta
+                        name="twitter:description"
+                        content="A comprehensive tool for developers to compare and and do in-depth analysis of the expanding Ethereum ecosystem"
+                    />
+                    <meta
+                        name="twitter:image"
+                        content={`https://${
+                            process.env.NEXT_PUBLIC_VERCEL_URL ||
+                            process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL
+                        }/images/thumbnail.png`}
+                    />
+                    <meta name="robots" content="index, follow" />
+                    <meta
+                        name="viewport"
+                        content="width=device-width, initial-scale=1"
+                    />
+                    c
+                </Head>
+                <style jsx global>{`
+                    :root {
+                        --font-chakra-petch: ${chakraPetch.style.fontFamily};
+                        --font-roboto-mono: ${robotoMono.style.fontFamily};
+                    }
+                    html {
+                        font-family: ${inter.style.fontFamily};
+                    }
+                `}</style>
+                <main className={classNames('main')}>
+                    <Component {...pageProps} />
+                </main>
+            </ThemeProvider>
+        </>
     )
 }
