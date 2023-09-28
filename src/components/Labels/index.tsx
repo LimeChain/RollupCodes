0a31ade14f094e16bacd653b281b4a61d1d4665e
Mihail Kirov <mihail.kirov@limechain.tech>
2023-09-28 11:52:01 +0300

    feat: scroll docs

--- src/components/Labels/index.tsx
+++ src/components/Labels/index.tsx
@@ -10,6 +10,7 @@ const rollupsFirstColors: Record<string, string> = {
     arbitrum_one: '#F59762',
     optimism: '#F59762',
     polygon_zkevm: '#7A8FFF',
+    scroll: '#7A8FFF',
     zksync_era: '#7A8FFF',
 }
 
