8ec6fda4379452cfdc225035a83af390f1e9c52a
Deyan Dimitrov <dikelito@tutamail.com>
2025-03-28 10:38:55 +0200

    feat: Change Twitter icon and references to X (#140)
    Signed-off-by: dikel <dikelito@tutamail.com>

--- src/components/Icon/icons.tsx
+++ src/components/Icon/icons.tsx
@@ -2,7 +2,7 @@ import WebsiteIcon from '/public/images/website-icon.svg'
 import DocsIcon from '/public/images/docs-icon.svg'
 import L2BeatIcon from '/public/images/l2beat-icon.svg'
 import GithubIcon from '/public/images/github-icon.svg'
-import TwitterIcon from '/public/images/twitter-icon.svg'
+import XIcon from '/public/images/x-icon.svg'
 import DiscordIcon from '/public/images/discord-icon.svg'
 
 const icons: Record<string, any> = {
@@ -10,7 +10,7 @@ const icons: Record<string, any> = {
     docs: <DocsIcon fill={'var(--text-color)'} />,
     l2beat: <L2BeatIcon fill={'var(--text-color)'} />,
     github: <GithubIcon fill={'var(--text-color)'} />,
-    twitter: <TwitterIcon fill={'var(--text-color)'} />,
+    x: <XIcon fill={'var(--text-color)'} />,
     discord: <DiscordIcon fill={'var(--text-color)'} />,
 }
 
